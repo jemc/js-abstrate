de40880fe6614cd483b1b7c46e6e503bd3d32afe
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-11-22 00:23:53 -0800

    Favor "node" terminology over "term" in the interpreter.

--- src/go.interpret.js
+++ src/go.interpret.js
@@ -3,53 +3,53 @@
 const interpret = {}
 module.exports = interpret
 
-// To interpret a body, interpret the terms then flatten them to a string value.
-interpret.body = (terms, data, runtime) => {
+// To interpret a body, interpret the nodes then flatten them to a string value.
+interpret.body = (nodes, data, runtime) => {
   let value = ""
-  interpret.terms(terms, data, runtime).forEach((chunk) => {
+  interpret.nodes(nodes, data, runtime).forEach((chunk) => {
     value = value + chunk.value
   })
-  return { value: value, from: terms }
+  return { value: value, from: nodes }
 }
 
-// To interpret a list of terms, interpret each term and return the list.
-interpret.terms = (terms, data, runtime) => {
-  return terms.map((term) => { return interpret.term(term, data, runtime) })
+// To interpret a list of nodes, interpret each node and return the list.
+interpret.nodes = (nodes, data, runtime) => {
+  return nodes.map((node) => { return interpret.node(node, data, runtime) })
 }
 
-// To interpret a term, delegate to the type-specific function for that term.
-interpret.term = (term, data, runtime) => {
-  if (term.type in interpret) {
-    return { value: interpret[term.type](term, data, runtime), from: term }
+// To interpret a node, delegate to the type-specific function for that node.
+interpret.node = (node, data, runtime) => {
+  if (node.type in interpret) {
+    return { value: interpret[node.type](node, data, runtime), from: node }
   } else {
-    throw new Error("NOT IMPLEMENTED: go.interpret for type: " + term.type)
+    throw new Error("NOT IMPLEMENTED: go.interpret for type: " + node.type)
   }
 }
 
-// A text node returns the raw text content of the term (a string).
+// A text node returns the raw text content of the node (a string).
 // The trimLeft/trimRight options specify to trim leading/trailing whitespace.
-interpret.text = (term, data, runtime) => {
-  let value = term.content
-  if (term.trimLeft) { value = value.replace(/^\s+/, "") }
-  if (term.trimRight) { value = value.replace(/\s+$/, "") }
+interpret.text = (node, data, runtime) => {
+  let value = node.content
+  if (node.trimLeft) { value = value.replace(/^\s+/, "") }
+  if (node.trimRight) { value = value.replace(/\s+$/, "") }
   return value
 }
 
 // A root node simply returns the root of the data passed in.
-interpret.root = (term, data, runtime) => {
+interpret.root = (node, data, runtime) => {
   return data
 }
 
 // An if block interprets either the body or elseBody, based on its term.
-interpret.if = (term, data, runtime) => {
-  if (interpret.term(term.term, data, runtime).value) {
-    return interpret.body(term.body, data, runtime).value
+interpret.if = (node, data, runtime) => {
+  if (interpret.node(node.term, data, runtime).value) {
+    return interpret.body(node.body, data, runtime).value
   } else {
-    return interpret.body(term.elseBody, data, runtime).value
+    return interpret.body(node.elseBody, data, runtime).value
   }
 }
 
 // A dot node returns the named member of the data passed in.
-interpret.dot = (term, data, runtime) => {
-  return interpret.term(term.of, data, runtime).value[term.name]
+interpret.dot = (node, data, runtime) => {
+  return interpret.node(node.of, data, runtime).value[node.name]
 }
