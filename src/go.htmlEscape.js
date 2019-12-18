5ad2444f54e0fdaf4cc87df1e6b3183882adda9d
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-12-17 16:30:12 -0800

    fix: URI attribute context-aware escaping in `go.htmlEscape`.

--- src/go.htmlEscape.js
+++ src/go.htmlEscape.js
@@ -1,6 +1,42 @@
 "use strict"
+const hyntax = require("hyntax")
 
 const internal = {}
+
+module.exports = (template) => {
+  // First, tokenize the template string into HTML syntax tokens.
+  // These will be used to figure out where we are (syntactically) in the HTML.
+  const tokens = hyntax.tokenize(template).tokens
+
+  return (string, node) => {
+    // Find the first token which encompasses the begin offset of this AST node.
+    // We also have some closure side-effects here for tracking some state
+    // based on recent tokens occurring just prior to the token that we find.
+    var lastAttrName
+    const token = tokens.find((token) => {
+      // As a side-effect, capture the attribute name if this token is one.
+      if (token.type == "token:attribute-key") {
+        lastAttrName = token.content
+      }
+
+      // Return true if this token encompasses the beginning of this AST node.
+      return token.endPosition > node.beginOffset
+        && token.startPosition < node.beginOffset
+    })
+
+    // Based on the token type we found, use the correct escaping approach.
+    switch (token.type) {
+      // Attribute values are escaped based on the name of the attribute.
+      case "token:attribute-value":
+        return internal.fnForAttrName(lastAttrName)(string)
+
+      // Normal text and everything else are escaped "normally" (for HTML).
+      case "token:text":
+      default:
+        return internal.htmlEscape(string)
+    }
+  }
+}
 
 internal.htmlEscape = (string) => {
   return string
@@ -13,8 +49,65 @@ internal.htmlEscape = (string) => {
     .replace(/>/g, "&gt;")
 }
 
-module.exports = (template) => {
-  return (string, node) => {
-    return internal.htmlEscape(string)
+internal.uriEscape = (string) => {
+  const replacePattern = /(?!%\h\h)[^A-Za-z0-9\-._~!#$&*+,/:;=?@[\]]/g
+  return string.replace(replacePattern, (c) => {
+    return "%" + c.charCodeAt(0).toString(16);
+  })
+}
+
+internal.fnForAttrName = (attrName) => {
+  // See https://www.w3.org/TR/html5/Overview.html#attributes-1
+  // See https://www.w3.org/TR/html4/index/attributes.html
+  switch (attrName) {
+    case "action":
+    case "archive":
+    case "background":
+    case "cite":
+    case "classid":
+    case "codebase":
+    case "data":
+    case "formaction":
+    case "href":
+    case "icon":
+    case "longdesc":
+    case "manifest":
+    case "poster":
+    case "profile":
+    case "src":
+    case "usemap":
+    case "xmlns":
+      return internal.uriEscape
+    case "accept-charset":
+    case "async":
+    case "challenge":
+    case "charset":
+    case "content":
+    case "crossorigin":
+    case "defer":
+    case "enctype":
+    case "form":
+    case "formenctype":
+    case "formmethod":
+    case "formnovalidate":
+    case "http-equiv":
+    case "keytype":
+    case "language":
+    case "method":
+    case "novalidate":
+    case "pattern":
+    case "rel":
+    case "sandbox":
+    case "type":
+    case "value":
+      return internal.htmlEscape // TODO: "unsafe" escaping
+    case "srcdoc":
+      return internal.htmlEscape // TODO: no escaping?
+    case "srcset":
+      return internal.htmlEscape // TODO: special-case srcset escaping
+    case "style":
+      return internal.htmlEscape // TODO: CSS escaping
+    default:
+      return internal.htmlEscape
   }
 }
--- test/go.htmlEscape.test.js
+++ test/go.htmlEscape.test.js
@@ -16,12 +16,33 @@ describe("Abstrate.go.htmlEscape", () => {
   // so the actual output was captured and verified using this sandbox:
   //   https://play.golang.org/p/Snlfy8bQg8g
 
-  it("escapes in an html content context", () => {
+  it("escapes in an HTML content context", () => {
     const result = render(
-      "{{.}}",
+      `{{.}}`,
       "O'Reilly: How are <i>you</i>?",
     )
-    assert.equal(result, "O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?")
+    assert.equal(result,
+      `O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?`)
+  })
+
+  it("escapes in an HTML attribute context", () => {
+    const result = render(
+      `<a title="{{.}}">`,
+      "O'Reilly: How are <i>you</i>?",
+    )
+    assert.equal(result,
+      `<a title="O&#39;Reilly: How are &lt;i&gt;you&lt;/i&gt;?">`
+    )
+  })
+
+  it("escapes in an HTML URI attribute context", () => {
+    const result = render(
+      `<a href="/{{.}}">`,
+      "O'Reilly: How are <i>you</i>?",
+    )
+    assert.equal(result,
+      `<a href="/O%27Reilly:%20How%20are%20%3ci%3eyou%3c/i%3e?">`
+    )
   })
 
   it("is fully compatible with how the real Go HTML escaping works")
